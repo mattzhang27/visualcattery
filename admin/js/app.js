380081d1a57df951d1ea9b0528641466bc418ec9
mattzhang27 <mattzhang27@outlook.com>
2019-04-08 15:38:24 +0800

    修改请求地址

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,3 +1,3 @@
 // var Host = "http://127.0.0.1:8080"
-var Host = "http://api.visualcattery.com/visualcattery"
+var Host = "http://106.12.31.60:8080/visualcattery"
 var HostImg = "http://img.visualcattery.com/"
